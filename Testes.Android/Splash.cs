f20c5eab31340ac018bfe6918db3a58c96c3a2c3
Douglas <douglasalves395@gmail.com>
2019-08-14 17:11:02 -0300

    Commit inicial

---
+++ Testes.Android/Splash.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Graphics.Drawables;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Content.Res;
+using Android.Util;
+using static Android.App.ActionBar;
+
+namespace Testes.Droid
+{
+    [Activity(Label = "Splash", Theme ="@style/Splash", MainLauncher = true, NoHistory = true)]
+    public class Splash : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
